79c8ab4c23fd5328bf02f3cd91ca3872efb0e224
Kimmi17 <dangthikimthuybl@gmail.com>
2023-08-29 02:32:13 +0300

    Creating individual product screen.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import getCategories from "@/actions/get-categories";
 import NavbarActions from "./navbar-actions";
 
+export const revalidate = 0;
+
 const Navbar = async () => {
   const categories = await getCategories();
   return (
